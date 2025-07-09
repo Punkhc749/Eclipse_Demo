8f67959419273c6c05ad35d968a2467c5530ce3b
Punkhc <punkhc749@gmail.com>
2025-07-09 14:35:23 +0300

    Add feature (sqrt)

--- Git_Demo/src/Demo/Calculator.java
+++ Git_Demo/src/Demo/Calculator.java
@@ -1,9 +1,7 @@
 package Demo;
 
 public class Calculator {
-	public static void main(String[] args) {
-
-	}
+	
 
 	public static int add(int a, int b) {
 		return a + b;
@@ -13,10 +11,5 @@ public class Calculator {
 		return a - b;
 	}
 
-	public double sqrt(double a) {
-		if (a < 0) {
-			throw new IllegalArgumentException("Cannot calculate square roof of negative number ");
-		}
-		return Math.sqrt(a);
-	}
+	
 }
