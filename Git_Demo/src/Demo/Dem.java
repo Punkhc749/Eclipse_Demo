d785bc977575f5149892e902b6c022f7c86a227e
Punkhc <punkhc749@gmail.com>
2025-07-09 13:59:06 +0300

    New sysou

--- Git_Demo/src/Demo/Dem.java
+++ Git_Demo/src/Demo/Dem.java
@@ -3,6 +3,7 @@ package Demo;
 public class Dem {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git"); // first init
+		System.out.println("Hello Git 2 Init"); // second init
 	}
 }
