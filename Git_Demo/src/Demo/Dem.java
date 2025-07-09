a2dbfb9d511054785c992b5b221af2e04c0ed444
Punkhc <punkhc749@gmail.com>
2025-07-09 14:33:46 +0300

    1

--- Git_Demo/src/Demo/Dem.java
+++ Git_Demo/src/Demo/Dem.java
@@ -5,6 +5,6 @@ public class Dem {
 	public static void main(String[] args) {
 		System.out.println("Hello Git"); // first init
 		System.out.println("Hello Git 2 Init"); // second init
-		System.out.println("Git pull changes"): // init for GitHub
+		System.out.println("Git pull changes"); // init for GitHub
 	}
 }
