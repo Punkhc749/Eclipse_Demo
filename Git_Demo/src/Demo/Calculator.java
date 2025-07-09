fa3fa3c9e1a2f995fa8fc828cceb58852c3ce8c4
Punkhc <punkhc749@gmail.com>
2025-07-09 15:40:23 +0300

    Create new-feature branc(and add sysout)

--- Git_Demo/src/Demo/Calculator.java
+++ Git_Demo/src/Demo/Calculator.java
@@ -3,6 +3,7 @@ package Demo;
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("DEMO");
+		
 	}
 
 	public static int add(int a, int b) {
@@ -10,7 +11,10 @@ public class Calculator {
 	}
 
 	public static int subtract(int a, int b) {
-		return a - b; 
+		return a - b;
+	}
+	public void print() {
+		System.out.println("This is in a new feature");
 	}
 
 }
